12cb10799a2c9082942035c962473faac5b56f03
Smith <smithwebdd@gmail.com>
2019-08-13 18:42:09 -0400

    Updated new and edit devise views, added navbar sign in/out options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_154104) do
+ActiveRecord::Schema.define(version: 2019_08_13_212723) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2019_08_13_154104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
