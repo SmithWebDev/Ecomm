d8894fc5698499293e5e7ec78fc6c4ff01ab5f04
Smith <smithwebdd@gmail.com>
2019-08-13 22:41:40 -0400

    Updated Navbar with Items by brand

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :categories
+  before_action :categories, :brands
 
   def categories
     @categories = Category.order(:name)
+  end
+
+  def brands
+    @brands = Product.pluck(:brand).sort.uniq
   end
 
   protected
