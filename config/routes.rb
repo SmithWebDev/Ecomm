d366fafb6b82ace93c702977105ca5dda11390b1
Smith <smithwebdd@gmail.com>
2019-08-14 11:38:42 -0400

    Created view order and checkout views, update product loop with add to cart function, updated navbar with view items link

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get 'categorical'  => 'storefront#items_by_category'
   get 'branding'     => 'storefront#items_by_brand'
 
-  get  'view_order'  => 'cart/view_order'
-  get  'checkout'    => 'cart/checkout'
-  post "add_to_cart" => 'cart/add_to_cart'
+  get  'view_order'  => 'cart#view_order'
+  get  'checkout'    => 'cart#checkout'
+  post "add_to_cart" => 'cart#add_to_cart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
