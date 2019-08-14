f9b6d6e38816bbea11984fad94029fc3558b08dc
Smith <smithwebdd@gmail.com>
2019-08-14 10:39:21 -0400

    Updated routes with custom routes for Cart actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'storefront#all_items'
-  get 'categorical' => 'storefront#items_by_category'
-  get 'branding'    => 'storefront#items_by_brand'
   resources :products
-  get 'cart/view_order'
-  get 'cart/checkout'
-  get 'cart/add_to_cart'
+
   #root 'products#index'
   get 'products/index'
+
+  root 'storefront#all_items'
+  get 'categorical'  => 'storefront#items_by_category'
+  get 'branding'     => 'storefront#items_by_brand'
+
+  get  'view_order'  => 'cart/view_order'
+  get  'checkout'    => 'cart/checkout'
+  post "add_to_cart" => 'cart/add_to_cart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
